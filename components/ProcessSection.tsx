dcc9b4fdd4b58f454ada98813e818589c17f5589
g <g@gs-macbook-pro.local>
2025-11-08 16:22:37 -0600

    Make narrowing diagram blend into page background on all narrowing pages

--- components/ProcessSection.tsx
+++ components/ProcessSection.tsx
@@ -4,21 +4,16 @@ import Image from "next/image";
 
 export default function ProcessSection() {
   return (
-    <section className="mx-auto my-12 w-full max-w-4xl rounded-2xl bg-zinc-50 px-6 py-10 sm:px-10 sm:py-14">
-      <div className="text-center">
-        <div className="flex justify-center">
-          {/* Narrowing process graphic showing workflow */}
-          <Image
-            src="/choosie-process.svg"
-            alt="Choosie process: Programmer, Curator, Selector, Decider"
-            width={1152}
-            height={768}
-            priority={false}
-            unoptimized
-            className="h-auto w-full max-w-[34rem] select-none"
-          />
-        </div>
-      </div>
-    </section>
+    <div className="my-10 w-full flex justify-center">
+      <Image
+        src="/choosie-process.svg"
+        alt="Choosie process: Programmer, Curator, Selector, Decider"
+        width={1152}
+        height={768}
+        priority={false}
+        unoptimized
+        className="h-auto w-full max-w-[34rem] select-none"
+      />
+    </div>
   );
 }
