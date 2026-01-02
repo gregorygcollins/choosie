f395b743696d830a01ef0401079575e70e5695d0
g <g@gs-macbook-pro.local>
2026-01-01 18:26:54 -0600

    chore: migrate Next.js config to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,39 @@
+// next.config.mjs
+
+const isProd = process.env.NODE_ENV === "production";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
+  async headers() {
+    const headers = [
+      { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+      { key: "X-Frame-Options", value: "DENY" },
+      { key: "X-Content-Type-Options", value: "nosniff" },
+      { key: "X-XSS-Protection", value: "0" },
+      {
+        key: "Permissions-Policy",
+        value:
+          "camera=(), microphone=(), geolocation=(), interest-cohort=(), browsing-topics=()",
+      },
+    ];
+    if (isProd) {
+      headers.push({
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+      });
+    }
+    return [
+      {
+        source: "/(.*)",
+        headers,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
